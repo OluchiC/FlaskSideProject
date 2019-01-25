b1e0ddb22960dfb3b8240860c81cdb4d96f3d8f5
OluchiC <Oluchi.j.c@Gmail.com>
2019-01-25 18:27:12 -0500

    Got it to display and changed some things

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+#Import Flask and Imported the render_template method from
+#the Flask framework
 
+from flask import Flask, render_template
+#Flask  is the prototype used to create instances
+#of web application or web application
 app = Flask(__name__)
 
+#Add different routes
 @app.route('/')
 def main():
+    #you have to pass in a HTML File
     return render_template('index.html')
 
+@app.route('/about/')
+def about():
+    #this will take you to the aboutMe Homepage
+    return render_template('aboutMe.html')
+
 if __name__ == '__main__':
+    #print out Python errors on the web page
+    # helping us trace the errors
     app.run(debug=True)
